e470265fb4c916f71fbcef2156feab5381db5d14
kristianleoruth <kristianleoruth@gmail.com>
2024-04-18 23:59:53 +0200

    Reworked to Object Model

---
+++ game-master.js
@@ -0,0 +1,75 @@
+import $ from "jquery"
+import * as Three from "three"
+import * as Util from "./util.js"
+
+export class GameMaster {
+  constructor(window) { 
+    this.window = window
+    this.FRAME_TIME = 0.1
+    this.prevRender = null
+  }
+
+  _InitEvents() {
+    /* Events */
+    $(window).on("resize", () => {
+      this.mainCamera.aspect = window.innerWidth / window.innerHeight
+      this.mainCamera.updateProjectionMatrix()
+      renderer.setSize(window.innerWidth, window.innerHeight)
+    })
+
+    $(window).on("DOMMouseScroll mousewheel", e => {
+      e.preventDefault()
+    })
+  }
+
+  _InitGame() {
+    const canvas = document.querySelector("canvas.webgl")
+    this.scene = new Three.Scene()
+    
+    this.scene.background = new Three.Color(0x0)
+    
+    this.renderer = new Three.WebGLRenderer({ canvas: canvas, antialias: true })
+    this.renderer.setSize(window.innerWidth, window.innerHeight)
+    this.tloader = new Three.TextureLoader()
+  
+    this.startTime = Date.now()
+  
+    this.mainCamera = new Three.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
+    this.scene.add(this.mainCamera)
+  
+    this.originToCam = new Three.Vector2(0,0)
+    this.frameDelta = 0.1
+    this.prevRender = Date.now()
+  }
+
+  // sets frameDelta, prevRender
+  _SetFrameTimes() {
+    this.frameDelta = (Date.now() - this.prevRender) * 0.001 // convert to seconds
+    this.prevRender += this.frameDelta
+  }
+
+  /* Render environment scene */
+  _Render() {
+    this.renderer.render(this.scene, this.mainCamera)
+  }
+
+  Init() {
+    this._InitGame()
+    this._InitEvents()
+    this.CustomStartupLogic()
+  }
+
+  CustomStartupLogic = () => {}
+  CustomRuntimeLogic = () => {}
+}
+
+const GameLoop = (gameMaster) => {
+  gameMaster.CustomRuntimeLogic()
+  gameMaster._Render()
+  gameMaster._SetFrameTimes()
+}
+
+export const SetupGameLoop = (gameMaster) => {
+  window.setInterval(GameLoop, gameMaster.FRAME_TIME, gameMaster)
+}
+
--- game.js
+++ game.js
@@ -1,58 +1,18 @@
-import $ from "jquery"
 import * as Three from "three"
-import * as Util from "./util.js"
+import $ from "jquery"
+import Util from "./util.js"
+import * as GameMaster from "./game-master.js"
 
-let env = import.meta.env
+let gm = new GameMaster.GameMaster(window)
+gm.CustomStartupLogic = Start
+gm.Init()
+gm.CustomRuntimeLogic = Game
+GameMaster.SetupGameLoop(gm)
 
-function InitGame() {
-  const canvas = document.querySelector("canvas.webgl")
-  env.scene = new Three.Scene()
-  
-  env.scene.background = new Three.Color(0x0)
-  
-  env.renderer = new Three.WebGLRenderer({ canvas: canvas, antialias: true })
-  
-  env.tloader = new Three.TextureLoader()
-  env.renderer.setSize(window.innerWidth, window.innerHeight)
-
-  env.startTime = Date.now()
-
-  env.mainCamera = new Three.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
-  env.scene.add(env.mainCamera)
-
-  env.originToCam = new Three.Vector2(0,0)
-  env.frameDelta = 0.1
-  env.prevRender = Date.now()
+function Start() {
+  // console.log("I play once at startup")
 }
 
-// Start game
-InitGame()
-
-// sets frameDelta, prevRender
-function SetFrameTimes() {
-  env.frameDelta = (Date.now() - env.prevRender) * 0.001 // convert to seconds
-  env.prevRender += env.frameDelta
+function Game() {
+  // console.log("I play every frame")
 }
-
-/* Render environment scene */
-function Render() {
-  env.renderer.render(env.scene, env.mainCamera)
-}
-
-function GameLoop() {
-  Render()
-  SetFrameTimes()
-}
-
-window.setInterval(GameLoop, env.FRAME_TIME)
-
-/* Events */
-$(window).on("resize", () => {
-  camera.aspect = window.innerWidth / window.innerHeight
-  camera.updateProjectionMatrix()
-  renderer.setSize(window.innerWidth, window.innerHeight)
-})
-
-$(window).on("DOMMouseScroll mousewheel", e => {
-  e.preventDefault()
-})
