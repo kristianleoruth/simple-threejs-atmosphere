9fad37b47d0ae21a22ac582faa94739cb6e1da8f
kristianleoruth <kristianleoruth@gmail.com>
2024-04-18 21:47:08 +0200

    Readability

--- game.js
+++ game.js
@@ -2,26 +2,26 @@ import $ from "jquery"
 import * as Three from "three"
 import * as Util from "./util.js"
 
-console.log(import.meta.env)
+let env = import.meta.env
 
 function InitGame() {
   const canvas = document.querySelector("canvas.webgl")
-  import.meta.env.scene = new Three.Scene()
+  env.scene = new Three.Scene()
   
-  import.meta.env.scene.background = new Three.Color(0x0)
+  env.scene.background = new Three.Color(0x0)
   
-  import.meta.env.renderer = new Three.WebGLRenderer({ canvas: canvas, antialias: true })
+  env.renderer = new Three.WebGLRenderer({ canvas: canvas, antialias: true })
   
-  import.meta.env.tloader = new Three.TextureLoader()
-  import.meta.env.renderer.setSize(window.innerWidth, window.innerHeight)
+  env.tloader = new Three.TextureLoader()
+  env.renderer.setSize(window.innerWidth, window.innerHeight)
 
-  import.meta.env.startTime = Date.now()
+  env.startTime = Date.now()
 
-  import.meta.env.mainCamera = new Three.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
+  env.mainCamera = new Three.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
 
-  import.meta.env.originToCam = new Three.Vector2(0,0)
-  import.meta.env.frameDelta = 0.1
-  import.meta.env.prevRender = Date.now()
+  env.originToCam = new Three.Vector2(0,0)
+  env.frameDelta = 0.1
+  env.prevRender = Date.now()
 }
 
 // Start game
@@ -29,23 +29,23 @@ InitGame()
 
 // sets frameDelta, prevRender
 function SetFrameTimes() {
-  import.meta.env.frameDelta = (Date.now() - import.meta.env.prevRender) * 0.001 // convert to seconds
-  import.meta.env.prevRender += import.meta.env.frameDelta
+  env.frameDelta = (Date.now() - env.prevRender) * 0.001 // convert to seconds
+  env.prevRender += env.frameDelta
 }
 
 /* Render environment scene */
 function Render() {
-  import.meta.env.renderer.render(import.meta.env.scene, import.meta.env.mainCamera)
+  env.renderer.render(env.scene, env.mainCamera)
 }
 
 function GameLoop() {
-  console.log(`Previous render: ${import.meta.env.prevRender}\nFrame Delta: ${import.meta.env.frameDelta}`)
+  console.log(`Previous render: ${env.prevRender}\nFrame Delta: ${env.frameDelta}`)
   
   Render()
   SetFrameTimes()
 }
 
-window.setInterval(GameLoop, import.meta.env.FRAME_TIME)
+window.setInterval(GameLoop, env.FRAME_TIME)
 
 /* Events */
 $(window).on("resize", () => {
